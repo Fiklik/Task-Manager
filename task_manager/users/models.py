5793233c486d65f1924af378bacb3edbcd7ad826
Aleksey Yablunev <yablunev.av@gmail.com>
2023-12-01 15:42:28 +0300

    merged branches

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-from django.contrib.auth.models import User
-
-
-class User(User):
-=======
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
->>>>>>> new-branch
     def __str__(self):
         return self.get_full_name()
